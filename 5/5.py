a5ef6b3b42814e146ed8bd8999c6371a12441c06
BrunonDEV <brunonblok@gmail.com>
2024-04-07 20:26:45 +0200

    5 - bruteforce detection, simplify patterns

--- 5/5.py
+++ 5/5.py
@@ -22,9 +22,14 @@ parser_print_parsed = sub_parsers.add_parser("print_parsed", help="Print parsed 
 parser_print_random_user = sub_parsers.add_parser("random_user", help="Print n random logs for a random user")
 parser_print_avg_session_duration = sub_parsers.add_parser("avg_session_duration", help="Print average session duration")
 parser_print_most_least_logged_in_user = sub_parsers.add_parser("most_least_logged_in_user", help="Print most and least logged in user")
+parser_detect_bruteforce = sub_parsers.add_parser("detect_bruteforce", help="Detect bruteforce")
 
 parser_print_random_user.add_argument("-n", "--number", type=int, help="Number of logs to print", required=True)
 parser_print_avg_session_duration.add_argument("-u", "--user", type=str, help="User to calculate average session duration for", required=False)
+
+parser_detect_bruteforce.add_argument("-u", "--user", type=str, help="User to detect bruteforce for (default: all)", required=False)
+parser_detect_bruteforce.add_argument("-t", "--max_delay", type=int, help="Max delay between attempts in seconds", default=5)
+parser_detect_bruteforce.add_argument("-a", "--max_attempts", type=int, help="Max attempts", default=3)
 
 args = argparser.parse_args()
 # Argparse
@@ -32,7 +37,7 @@ args = argparser.parse_args()
 
 # Logger
 logger = logging.getLogger(__name__)
-logger.setLevel(50 - args.verbosity * 10)
+logger.setLevel(51 - args.verbosity * 10)
 
 h1 = logging.StreamHandler(sys.stdout)
 h1.setLevel(logging.DEBUG)
@@ -89,11 +94,11 @@ def get_user_from_log(entry: LogEntry) -> str | None:
 def get_message_type(entry: LogEntry) -> str:
     patterns = {
         "SessionOpened": r"pam_unix\(sshd:session\): session opened for user (\w+)",
-        "AuthFailure": r"pam_unix\(sshd:auth\): authentication failure; (\w+) rhost=(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})",
-        "WrongPassword": r"Failed password for (\w+) from (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})",
+        "AuthFailure": r"pam_unix\(sshd:auth\): authentication failure",
+        "WrongPassword": r"Failed password for (\w+) from",
         "SessionClosed": r"pam_unix\(sshd:session\): session closed for user (\w+)",
-        "WrongUsername": r"Invalid user (\w+) from (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})",
-        "PossibleBreakIn": r"reverse mapping checking getaddrinfo for (\w+\.\w+\.\w+\.\w+) failed - POSSIBLE BREAK-IN ATTEMPT! user=(\w+)"
+        "WrongUsername": r"Invalid user (\w+) from",
+        "PossibleBreakIn": r"failed - POSSIBLE BREAK-IN ATTEMPT!"
     }
     
     for key, pattern in patterns.items():
@@ -179,6 +184,42 @@ def print_parsed(entries: list[LogEntry]) -> None:
         print(f"(IP: {ip}) (User: {user}) (Message type: {message_type})")
 
 
+def detect_bruteforce(entries: list[LogEntry], max_delay: int, max_attempts: int, user_to_detect: str | None) -> dict[str, int]:
+    # Find the maximum chain of attempts for every IP
+    # If the chain is longer than max_attempts, it's a bruteforce attack
+    # If there's a delay longer than max_delay between attempts, it's a new chain
+    
+    longest_chains_for_ip: dict[str, int] = {}
+    current_chains_for_ip: dict[str, int] = {}
+    
+    last_time_for_ip: dict[str, datetime] = {}
+    
+    for log in entries:
+        ip = get_ipv4s_from_log(log)
+        user = get_user_from_log(log)
+        message_type = get_message_type(log)
+        
+        if user_to_detect and user != user_to_detect:
+            continue
+        
+        if ip:
+            if ip[0] not in longest_chains_for_ip:
+                longest_chains_for_ip[ip[0]] = 0
+                current_chains_for_ip[ip[0]] = 0
+            
+            if message_type == "AuthFailure":
+                if ip[0] not in last_time_for_ip or (log["date"] - last_time_for_ip[ip[0]]).total_seconds() > max_delay:
+                    current_chains_for_ip[ip[0]] = 0
+                
+                last_time_for_ip[ip[0]] = log["date"]
+                current_chains_for_ip[ip[0]] += 1
+                
+                if current_chains_for_ip[ip[0]] > longest_chains_for_ip[ip[0]]:
+                    longest_chains_for_ip[ip[0]] = current_chains_for_ip[ip[0]]
+                    
+    return dict(filter(lambda x: x[1] > max_attempts, longest_chains_for_ip.items()))
+
+
 def main() -> None:
     f = open("SSH.log", "r")
     entries = parse_all(f.readlines())
@@ -206,6 +247,11 @@ def main() -> None:
             
             print(f"Most logged in user: {most}")
             print(f"Least logged in user: {least}")
+        case "detect_bruteforce":
+            print(f">> Detect bruteforce - Max delay between attempts: {args.max_delay}s, Max attempts: {args.max_attempts}")
+            attacks = detect_bruteforce(entries, max_delay=args.max_delay, max_attempts=args.max_attempts, user_to_detect=args.user)
+            for ip, attempts in attacks.items():
+                print(f"Bruteforce attack detected from {ip} ({attempts} attempts)")
     
     f.close()
 
